f69b4bc0a0af1d4331a8d9b2cbd9f7d8a2d5ea97
Sergiu Spiridon <srg.mihai200059@gmail.com>
2026-06-12 13:07:51 +0000

    Remove unused corev1 dependency from platformagent_types

--- k8s-operator/api/v1alpha1/platformagent_types.go
+++ k8s-operator/api/v1alpha1/platformagent_types.go
@@ -17,7 +17,6 @@ limitations under the License.
 package v1alpha1
 
 import (
-	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
@@ -126,7 +125,6 @@ type PlatformAgentStatus struct {
 	// +optional
 	StorageStatus StorageStatus `json:"storageStatus,omitempty"`
 }
-
 
 // +kubebuilder:object:root=true
 // +kubebuilder:subresource:status
